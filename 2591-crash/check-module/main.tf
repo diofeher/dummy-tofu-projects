33c8b2e3313da211ec452bfacae7bf8a9bea7bac
Diogenes Fernandes <diofeher@gmail.com>
2025-03-15 10:22:09 -0300

    test version
    Signed-off-by: Diogenes Fernandes <diofeher@gmail.com>

--- 2591-crash/check-module/main.tf
+++ 2591-crash/check-module/main.tf
@@ -1,10 +1,22 @@
+
 check "kafka_cluster_health" {
-  data "http" "terraform_io" {
-    url = "https://www.terraform.io"
+  data "http" "tofu" {
+    url = "https://opentofu.org/"
   }
 
   assert {
-    condition     = data.http.terraform_io.status_code == 200
-    error_message = "${data.http.terraform_io.url} returned an unhealthy status code"
+    condition     = data.http.tofu.status_code == 200
+    error_message = "${data.http.tofu.url} returned an unhealthy status code"
   }
 }
+
+# # Valid config
+# resource "null_resource" "b" {
+# }
+
+
+# resource "null_resource" "a" {
+#   connection {
+#     host = null_resource.b.id
+#   }
+# }
